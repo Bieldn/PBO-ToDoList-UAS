d71fe376011a3283679d78952fbc143300ede1f5
LENOVO GAMING 3 <LENOVO GAMING 3@LAPTOP-NH6FGUR4>
2022-12-08 22:18:11 +0700

    Add project files.

---
+++ to do list PBO/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace to_do_list_PBO
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTgl = new System.Windows.Forms.TextBox();
+            this.txtKeg = new System.Windows.Forms.TextBox();
+            this.btnadd = new System.Windows.Forms.Button();
+            this.btnedit = new System.Windows.Forms.Button();
+            this.btndel = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.kegiatan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tanggal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.keterangan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(399, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(180, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "to do list";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "nama kegiatan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "tanggal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 247);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "keterangan";
+            // 
+            // txtName
+            // 
+            this.txtName.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtName.Location = new System.Drawing.Point(188, 146);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(428, 27);
+            this.txtName.TabIndex = 4;
+            this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
+            // 
+            // txtTgl
+            // 
+            this.txtTgl.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtTgl.Location = new System.Drawing.Point(188, 194);
+            this.txtTgl.Name = "txtTgl";
+            this.txtTgl.Size = new System.Drawing.Size(428, 27);
+            this.txtTgl.TabIndex = 5;
+            // 
+            // txtKeg
+            // 
+            this.txtKeg.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtKeg.Location = new System.Drawing.Point(188, 240);
+            this.txtKeg.Name = "txtKeg";
+            this.txtKeg.Size = new System.Drawing.Size(428, 27);
+            this.txtKeg.TabIndex = 6;
+            // 
+            // btnadd
+            // 
+            this.btnadd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnadd.Location = new System.Drawing.Point(672, 134);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(122, 51);
+            this.btnadd.TabIndex = 7;
+            this.btnadd.Text = "Add";
+            this.btnadd.UseVisualStyleBackColor = true;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnedit
+            // 
+            this.btnedit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnedit.Location = new System.Drawing.Point(817, 134);
+            this.btnedit.Name = "btnedit";
+            this.btnedit.Size = new System.Drawing.Size(122, 51);
+            this.btnedit.TabIndex = 8;
+            this.btnedit.Text = "Edit";
+            this.btnedit.UseVisualStyleBackColor = true;
+            this.btnedit.Click += new System.EventHandler(this.btnedit_Click);
+            // 
+            // btndel
+            // 
+            this.btndel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btndel.Location = new System.Drawing.Point(746, 216);
+            this.btndel.Name = "btndel";
+            this.btndel.Size = new System.Drawing.Size(122, 51);
+            this.btndel.TabIndex = 9;
+            this.btndel.Text = "Delete";
+            this.btndel.UseVisualStyleBackColor = true;
+            this.btndel.Click += new System.EventHandler(this.btndel_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv.BackgroundColor = System.Drawing.Color.Gainsboro;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.kegiatan,
+            this.tanggal,
+            this.keterangan});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dgv.Location = new System.Drawing.Point(0, 335);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 29;
+            this.dgv.Size = new System.Drawing.Size(1022, 188);
+            this.dgv.TabIndex = 11;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // kegiatan
+            // 
+            this.kegiatan.HeaderText = "Kegiatan";
+            this.kegiatan.MinimumWidth = 6;
+            this.kegiatan.Name = "kegiatan";
+            this.kegiatan.ReadOnly = true;
+            // 
+            // tanggal
+            // 
+            this.tanggal.HeaderText = "Tanggal";
+            this.tanggal.MinimumWidth = 6;
+            this.tanggal.Name = "tanggal";
+            this.tanggal.ReadOnly = true;
+            // 
+            // keterangan
+            // 
+            this.keterangan.HeaderText = "Keterangan";
+            this.keterangan.MinimumWidth = 6;
+            this.keterangan.Name = "keterangan";
+            this.keterangan.ReadOnly = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(654, 6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1022, 523);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btndel);
+            this.Controls.Add(this.btnedit);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtKeg);
+            this.Controls.Add(this.txtTgl);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtName;
+        private TextBox txtTgl;
+        private TextBox txtKeg;
+        private Button btnadd;
+        private Button btnedit;
+        private Button btndel;
+        private DataGridView dgv;
+        private PictureBox pictureBox1;
+        private DataGridViewTextBoxColumn kegiatan;
+        private DataGridViewTextBoxColumn tanggal;
+        private DataGridViewTextBoxColumn keterangan;
+    }
+}
